b2404f15dd23dfe32a72a08c955a42fd11cf9baa
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-07 21:57:24 +0800

    If ValueError

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -78,5 +78,6 @@ def calculator():
             if repeat_calculation not in ['y', 'n']:
                 raise ValueError("\n\033[91mValueError: Invalid input. Please enter 'y' for Yes or 'n' for No.\033[0m")
             break
+        except ValueError as exception:
         # 10. If they're finished, say "Thank you!" and end
 # 11. Call the main function to run the calculator
