0931c35f54d266377b3e0563bd23b859f5f2e29a
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-07 21:18:06 +0800

    Convert result to int

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -54,7 +54,7 @@ def calculator():
                 operation = "\033[95mDivision\033[0m"
             # 7. Ensure proper handling of any mistakes
             if result.is_integer():
-                result = int(result
+                result = int(result)
         # 8. Display the result and the exception
         # 9. Inquire if they want to do more math
         # 10. If they're finished, say "Thank you!" and end
