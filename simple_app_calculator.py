b1d8d2918aa3dd8ebefdbe08046224d28c07e90d
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-07 20:19:59 +0800

    Pseudocode

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -1,6 +1,6 @@
 # 1. Define the calculator function
 def calculator():
-# 2. Define the addition, subtraction, multiplication, division function
+    # 2. Define the addition, subtraction, multiplication, division function
     def add_numbers(first_number, second_number):
         return int(first_number + second_number) \
             if first_number.is_integer() and second_number.is_integer() \
@@ -13,7 +13,7 @@ def calculator():
         if second_number == 0:
             raise ZeroDivisionError()
         return first_number / second_number
-# 3. Making the main loop and display the operations
+    # 3. Making the main loop and display the operations
 # 4. Get the user's choice
 # 5. Get the numbers
 # 6. Perform the operation
