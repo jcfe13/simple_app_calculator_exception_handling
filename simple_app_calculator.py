233b6124233b50f905c013200b472fe552cba615
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-07 21:59:36 +0800

    Break loop if valid input

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -77,7 +77,7 @@ def calculator():
                 repeat_calculation = input("\n\033[93mDo you want to perform another calculation? ('y' for Yes / 'n' for No): ").strip().lower()
                 if repeat_calculation not in ['y', 'n']:
                     raise ValueError("\n\033[91mValueError: Invalid input. Please enter 'y' for Yes or 'n' for No.\033[0m")
-            break
+                break
         except ValueError as exception:
         # 10. If they're finished, say "Thank you!" and end
 # 11. Call the main function to run the calculator
