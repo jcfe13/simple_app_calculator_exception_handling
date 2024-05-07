f4cf97fc234d80cb7ef781f88050a3fe2b8fa339
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-07 19:59:49 +0800

    Pseudocode

---
+++ simple_app_calculator.py
@@ -0,0 +1,11 @@
+# 1. Define the calculator function
+# 2. Define the addition, subtraction, multiplication, division function
+# 3. Making the main loop and display the operations
+# 4. Get the user's choice
+# 5. Get the numbers
+# 6. Perform the operation
+# 7. Ensure proper handling of any mistakes
+# 8. Display the result and the exception
+# 9. Inquire if they want to do more math
+# 10. If they're finished, say "Thank you!" and end
+# 11. Call the main function to run the calculator
