9bf81b7af7319a97966c5c3c88a8d2fc3e286d8a
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-07 20:20:46 +0800

    Newline

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -1,18 +1,23 @@
 # 1. Define the calculator function
 def calculator():
+
     # 2. Define the addition, subtraction, multiplication, division function
     def add_numbers(first_number, second_number):
         return int(first_number + second_number) \
             if first_number.is_integer() and second_number.is_integer() \
             else first_number + second_number
+
     def subtract_numbers(first_number, second_number):
         return first_number - second_number
+
     def multiply_numbers(first_number, second_number):
         return first_number * second_number
+
     def divide_numbers(first_number, second_number):
         if second_number == 0:
             raise ZeroDivisionError()
         return first_number / second_number
+    
     # 3. Making the main loop and display the operations
 # 4. Get the user's choice
 # 5. Get the numbers
