616c99c16704d86d467ffb1e235748c39f0fc553
jcfe13 <johncarloesquillo72@gmail.com>
2024-05-07 21:35:25 +0800

    If any other error

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -65,6 +65,7 @@ def calculator():
             print("\n\033[91mValueError:", e, "\033[0m")
         except ZeroDivisionError:
             print("\n\033[91mZeroDivisionError: Division by zero is not a valid operation. 033[0m")
+        except Exception as exception:
         # 9. Inquire if they want to do more math
         # 10. If they're finished, say "Thank you!" and end
 # 11. Call the main function to run the calculator
